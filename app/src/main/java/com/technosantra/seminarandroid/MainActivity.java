ab2dd0fbb07f9ad5c199c3f70f91ec57f15691ba
Mohammad Nashrulloh <mohammad.nashrulloh@ansvia.com>
2019-04-05 23:16:31 +0700

    remove unused import

--- app/src/main/java/com/technosantra/seminarandroid/MainActivity.java
+++ app/src/main/java/com/technosantra/seminarandroid/MainActivity.java
@@ -1,34 +1,15 @@
 package com.technosantra.seminarandroid;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.preference.Preference;
-import android.provider.MediaStore;
-import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String username = sharedPref.getString("username", "");
                 String address = sharedPref.getString("address", "");
-                Toast.makeText(MainActivity.this, "Username is : "+username+" Address is : "+address, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Username is : " + username + " Address is : " + address, Toast.LENGTH_SHORT).show();
             }
         });
     }
