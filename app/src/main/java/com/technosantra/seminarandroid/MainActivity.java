85ca6724df83697aae414f592bbbb0ee9f347ffc
Mohammad Nashrulloh <mohammad.nashrulloh@ansvia.com>
2019-04-05 23:05:03 +0700

    camera & gallery

--- app/src/main/java/com/technosantra/seminarandroid/MainActivity.java
+++ app/src/main/java/com/technosantra/seminarandroid/MainActivity.java
@@ -1,20 +1,36 @@
 package com.technosantra.seminarandroid;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE = 2000;
+    private static final int REQUEST_CODE_CAMERA = 2000;
+    private static final int REQUEST_CODE_GALLERY = 3000;
+    File photoFile;
+    public String photoFileName = "photo.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,56 +39,120 @@ public class MainActivity extends AppCompatActivity {
         Log.d("Lifecycles", "onCreate");
 
         Button btnRequest = findViewById(R.id.btnRequest);
+        Button btnRequestGallery = findViewById(R.id.btnRequestGallery);
         btnRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("user", "Username");
-                intent.putExtra("age", "Age");
-                startActivityForResult(intent, REQUEST_CODE);
+                checkPermission();
+            }
+        });
+
+        btnRequestGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPickPhoto();
             }
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("Lifecycles", "onStart");
+    void checkPermission(){
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.CAMERA)) {
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
+            }
+        }else{
+            onLaunchCamera();
+        }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("Lifecycles", "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("Lifecycles", "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("Lifecycles", "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("Lifecycles", "onDestroy");
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK){
-            if (requestCode == REQUEST_CODE){
-                String username = data.getStringExtra("user");
-                String age = data.getExtras().getString("age");
-                Toast.makeText(this, "Username is :"+username+" Age is : "+age, Toast.LENGTH_SHORT).show();
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    onLaunchCamera();
+                } else {
+                    Log.d("CAMERA", "Permission Disable");
+                }
+                return;
             }
         }
     }
+
+    public void onLaunchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        photoFile = getPhotoFileUri(photoFileName);
+
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.technosantra.seminarandroid.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_CODE_CAMERA);
+        }
+    }
+
+    public void onPickPhoto() {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, REQUEST_CODE_GALLERY);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //Create a File Refrence
+    public File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "TAG");
+
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d("CAMERA", "failed to create directory");
+        }
+
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_CAMERA) {
+            if (resultCode == RESULT_OK) {
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                ImageView imgPreview = (ImageView) findViewById(R.id.imgPreview);
+                imgPreview.setImageBitmap(takenImage);
+            } else {
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }else if (requestCode == REQUEST_CODE_GALLERY){
+            Uri photoUri = data.getData();
+            Bitmap selectedImage = null;
+            try {
+                selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImageView imgPreview = findViewById(R.id.imgPreview);
+            imgPreview.setImageBitmap(selectedImage);
+        }
+    }
+
 }
