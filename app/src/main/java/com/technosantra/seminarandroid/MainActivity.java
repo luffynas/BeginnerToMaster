367466de7b4491444c896f613eae453320997818
Mohammad Nashrulloh <mohammad.nashrulloh@ansvia.com>
2019-04-05 23:15:44 +0700

    shared preference

--- app/src/main/java/com/technosantra/seminarandroid/MainActivity.java
+++ app/src/main/java/com/technosantra/seminarandroid/MainActivity.java
@@ -1,15 +1,19 @@
 package com.technosantra.seminarandroid;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.Preference;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -19,6 +23,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -27,132 +32,39 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE_CAMERA = 2000;
-    private static final int REQUEST_CODE_GALLERY = 3000;
-    File photoFile;
-    public String photoFileName = "photo.jpg";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Lifecycles", "onCreate");
 
-        Button btnRequest = findViewById(R.id.btnRequest);
-        Button btnRequestGallery = findViewById(R.id.btnRequestGallery);
+        final SharedPreferences sharedPref = getSharedPreferences("seminar", Context.MODE_PRIVATE);
+
+        final EditText txtUsername = findViewById(R.id.txtUsername);
+        final TextInputEditText txtAddress = findViewById(R.id.txtAddress);
+
+        Button btnRequest = findViewById(R.id.btnSave);
         btnRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkPermission();
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("username", txtUsername.getText().toString());
+                editor.putString("address", txtAddress.getText().toString());
+                editor.apply();
+
+                txtUsername.setText("");
+                txtAddress.setText("");
             }
         });
 
-        btnRequestGallery.setOnClickListener(new View.OnClickListener() {
+        Button btnShow = findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onPickPhoto();
+                String username = sharedPref.getString("username", "");
+                String address = sharedPref.getString("address", "");
+                Toast.makeText(MainActivity.this, "Username is : "+username+" Address is : "+address, Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-    void checkPermission(){
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.CAMERA)) {
-            } else {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
-            }
-        }else{
-            onLaunchCamera();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_CAMERA: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    onLaunchCamera();
-                } else {
-                    Log.d("CAMERA", "Permission Disable");
-                }
-                return;
-            }
-        }
-    }
-
-    public void onLaunchCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        photoFile = getPhotoFileUri(photoFileName);
-
-        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.technosantra.seminarandroid.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_CODE_CAMERA);
-        }
-    }
-
-    public void onPickPhoto() {
-        // Create intent for picking a photo from the gallery
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Bring up gallery to select a photo
-            startActivityForResult(intent, REQUEST_CODE_GALLERY);
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-    //Create a File Refrence
-    public File getPhotoFileUri(String fileName) {
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "TAG");
-
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d("CAMERA", "failed to create directory");
-        }
-
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-        return file;
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE_CAMERA) {
-            if (resultCode == RESULT_OK) {
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                ImageView imgPreview = (ImageView) findViewById(R.id.imgPreview);
-                imgPreview.setImageBitmap(takenImage);
-            } else {
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }else if (requestCode == REQUEST_CODE_GALLERY){
-            Uri photoUri = data.getData();
-            Bitmap selectedImage = null;
-            try {
-                selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            ImageView imgPreview = findViewById(R.id.imgPreview);
-            imgPreview.setImageBitmap(selectedImage);
-        }
-    }
-
 }
